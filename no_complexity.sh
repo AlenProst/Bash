fd2aa657b0cfb5fc85a3501bb5368870712191c0
AlenProst <57231780+AlenProst@users.noreply.github.com>
2022-02-16 13:18:31 +0200

    Update no_complexity.sh

--- no_complexity.sh
+++ no_complexity.sh
@@ -8,7 +8,7 @@ if [[ $totalCount -gt 2 ]]
 then
 	echo "Complexity was not removed.Removint complexity..."
 	
-	sed -i 's/password.*sha512/password	[success=1 default=ignore]	pam_unix.so sha512 minlen=1/g'
+	sed -i 's/password.*sha512/password	[success=1 default=ignore]	pam_unix.so sha512 minlen=1/g' $file
 	
 	echo "Complexity has been removed."
 	
