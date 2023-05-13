ad2d2e39a201882a546eed6ffc5d4b9c113c9029
AlenProst <57231780+AlenProst@users.noreply.github.com>
2023-05-13 10:12:07 +0300

    Update no_complexity.sh

--- no_complexity.sh
+++ no_complexity.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
+file="/etc/pam.d/common-password"
 
-file=/etc/pam.d/common-password
-totalCount=$(grep -o -i 'obscure' $file | wc -l)
-
-if [[ $totalCount -gt 2 ]] 
-then
-	echo "Complexity was not removed.Removint complexity..."
-	
-	sudo sed -i 's/password.*sha512/password	[success=1 default=ignore]	pam_unix.so sha512 minlen=1/g' $file
-	
-	echo "Complexity has been removed."
-	
-	
-else 
-	echo "Complexity has already been removed" 
-	
-	
+if grep -q "minlen=1" $file; then
+    echo "minlen=1 already present in $file"
+else
+    sed -i 's/obscure/minlen=1/g' $file
+    echo "minlen=1 added to $file"
 fi
